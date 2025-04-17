a41ea8ce569a649dc0297b5ee9f6e4f3e3ce478b
coltranegowan <coltranegowan@gmail.com>
2025-04-17 14:42:28 -0700

    lab 3 added

---
+++ global.js
@@ -0,0 +1,83 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// let navLinks = $$("nav a")
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+//   );
+
+// currentLink?.classList.add('current');
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume/', title: 'Resume' },
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/" 
+    : "/portfolio/"; 
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+
+    // Highlight current page
+    a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname
+    );
+
+    // Open external links in a new tab
+    a.toggleAttribute('target', a.host !== location.host);
+
+    nav.append(a);
+
+  }
+
+  // Create and insert color scheme switcher at top right
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <label class="color-scheme">
+      Theme:
+      <select id="color-scheme-select">
+        <option value="auto">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>
+    `
+  );
+  
+  let select = document.querySelector('#color-scheme-select');
+  
+  if ('colorScheme' in localStorage) {
+    document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+    select.value = localStorage.colorScheme;
+  }
+  
+  select.addEventListener('input', function (event) {
+    const scheme = event.target.value;
+    console.log('color scheme changed to', scheme);
+  
+    document.documentElement.style.setProperty('color-scheme', scheme);
+  
+    localStorage.colorScheme = scheme;
+  });
+  
+  

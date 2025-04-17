965b39f66757d9d12b78b7f01382e35b1a7c8324
coltranegowan <coltranegowan@gmail.com>
2025-04-17 14:49:51 -0700

    Update global.js

--- global.js
+++ global.js
@@ -24,7 +24,7 @@ document.body.prepend(nav);
 
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
     ? "/" 
-    : "/portfolio/"; 
+    : "https://coltranegowan.github.io/portfolio/"; 
 
 for (let p of pages) {
     let url = p.url;

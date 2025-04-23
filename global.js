9b8edf70269896a35e4e1d838796f78db151059f
coltranegowan <coltranegowan@gmail.com>
2025-04-23 16:45:56 -0700

    step 1.3

--- global.js
+++ global.js
@@ -1,5 +1,3 @@
-console.log('IT’S ALIVE!');
-
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
@@ -80,4 +78,17 @@ document.body.insertAdjacentHTML(
     localStorage.colorScheme = scheme;
   });
   
-  
+  export async function fetchJSON(url) {
+    try {
+      // Fetch the JSON file from the given URL
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      }
+      const data = await response.json();
+      return data;
+      console.log(response)
+    } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+    }
+  }
